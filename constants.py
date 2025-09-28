3df856cb60450d3fd82662af59b570eb393b1922
explodinggiraffes <john.koszarek@gmail.com>
2025-09-28 16:23:52 -0500

    Initial commit.

---
+++ constants.py
@@ -0,0 +1,12 @@
+from typing import Final
+
+
+WINDOW_WIDTH: Final[int] = 80
+WINDOW_HEIGHT: Final[int] = 50
+
+MAP_WIDTH: Final[int] = 80
+MAP_HEIGHT: Final[int] = 45
+
+ROOM_SIZE_MAX: Final[int] = 10
+ROOM_SIZE_MIN: Final[int] = 6
+ROOMS_MAX: Final[int] = 30
