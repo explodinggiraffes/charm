755e0f5486700470fa383487a7747fd37b865cba
explodinggiraffes <john.koszarek@gmail.com>
2025-12-29 14:50:35 -0600

    Refeactored RectangularRoom out of proc_gen/game_map_gen.py.

--- proc_gen/game_map_gen.py
+++ proc_gen/game_map_gen.py
@@ -7,36 +7,8 @@ import tcod
 
 from game_map import GameMap
 from graphical_block_characters import GraphicalBlockCharacters as Graphics
+from proc_gen.rectangular_room import RectangularRoom
 
-
-class RectangularRoom:
-    """A room within the dungeon."""
-    def __init__(self, x: int, y: int, width: int, height: int):
-        self.x1 = x
-        self.y1 = y
-        self.x2 = x + width
-        self.y2 = y + height
-
-    @property
-    def center(self) -> Tuple[int, int]:
-        center_x = int((self.x1 + self.x2) / 2)
-        center_y = int((self.y1 + self.y2) / 2)
-
-        return center_x, center_y
-
-    @property
-    def inner(self) -> Tuple[slice, slice]:
-        """Return the inner area of this room as a 2D array index."""
-        return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
-
-    def intersects(self, other: RectangularRoom) -> bool:
-        """Return True if this room overlaps with another RectangularRoom."""
-        return (
-            self.x1 <= other.x2
-            and self.x2 >= other.x1
-            and self.y1 <= other.y2
-            and self.y2 >= other.y1
-        )
 
 def generate_dungeon(
     max_rooms: int,
