8c711557b0521483c8114f4b2a4964adee945aa5
explodinggiraffes <john.koszarek@gmail.com>
2026-04-11 17:22:45 -0500

    Make internally used spawn functions private.

--- proc_gen/pawn_gen.py
+++ proc_gen/pawn_gen.py
@@ -4,19 +4,19 @@ from actors import Entity
 from constants import WINDOW_HEIGHT, WINDOW_WIDTH
 
 
-def spawn_player_actor() -> Entity:
-    """Return the entity representing the player character."""
-    return Entity("@", (255, 255, 255), 0, 0)
-
-def spawn_pawn() -> Entity:
-    """Return an entity controlled by the game's AI as non-player characters (NPCs)."""
-    return Entity("O", (255, 255, 0), int(WINDOW_WIDTH / 2 - 5), int(WINDOW_HEIGHT / 2))
-
 def spawn_pawns() -> List[Entity]:
     """Return a list of all pawns: the player character, as well as all NPCs."""
-    player = spawn_player_actor()
-    npc = spawn_pawn()
+    player = _spawn_player_actor()
+    npc = _spawn_pawn()
 
     entities = [player, npc]
 
     return entities
+
+def _spawn_player_actor() -> Entity:
+    """Return the entity representing the player character."""
+    return Entity("@", (255, 255, 255), 0, 0)
+
+def _spawn_pawn() -> Entity:
+    """Return an entity controlled by the game's AI as non-player characters (NPCs)."""
+    return Entity("O", (255, 255, 0), int(WINDOW_WIDTH / 2 - 5), int(WINDOW_HEIGHT / 2))
