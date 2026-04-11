5ced619bba20e7a66d8b3c89cf4226407ac89733
explodinggiraffes <john.koszarek@gmail.com>
2026-04-11 16:50:41 -0500

    Add npcs property and add_npc/remove_npc methods to Pawns class.

--- actors/pawns.py
+++ actors/pawns.py
@@ -35,3 +35,30 @@ class Pawns:
     @player.setter
     def player(self, value: Entity) -> None:
         self._player = value
+
+    @property
+    def npcs(self) -> List[Entity]:
+        """Get the list of non-player character entities."""
+        return self._npcs
+
+    def add_npc(self, npc: Entity) -> None:
+        """Add a non-player character entity to the list of NPCs.
+
+        Args:
+            npc: The entity to add to the NPC list.
+        """
+        self._npcs.append(npc)
+
+    def remove_npc(self, npc: Entity) -> None:
+        """Remove a non-player character entity from the list of NPCs.
+
+        If the supplied entity does not exist in the list of NPCs, no
+        action is taken.
+
+        Args:
+            npc: The entity to remove from the NPC list.
+        """
+        try:
+            self._npcs.remove(npc)
+        except ValueError:
+            pass
