a6475a9bb49fbcf66ad37a51d336e898f15a8bcc
explodinggiraffes <john.koszarek@gmail.com>
2026-04-11 17:38:16 -0500

    Remove re-exports of private spawn functions from proc_gen package.

--- proc_gen/__init__.py
+++ proc_gen/__init__.py
@@ -4,16 +4,10 @@ from proc_gen.game_map_gen import (
     generate_dungeon,
     tunnel_between,
 )
-from proc_gen.pawn_gen import (
-    spawn_pawn,
-    spawn_pawns,
-    spawn_player_actor,
-)
+from proc_gen.pawn_gen import spawn_pawns
 
 __all__ = [
     "generate_dungeon",
-    "spawn_pawn",
     "spawn_pawns",
-    "spawn_player_actor",
     "tunnel_between",
 ]
